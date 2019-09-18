59a4b9871c275baeb08b9471066ab799ca6367d7
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2019-09-17 21:36:05 -0300

    Recriação da classe SenhaUtils.

---
+++ src/main/java/br/com/meuprimeiroprojeto/senhautils/SenhaUtils.java
@@ -0,0 +1,35 @@
+package br.com.meuprimeiroprojeto.senhautils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SenhaUtils {
+	
+	/**
+	 * ​Gera​ ​um​ ​hash​ ​utilizando​ ​o​ ​BCrypt.
+	 * 
+	 * @param senha
+	 * @return String
+	 */
+	public static String gerarBcrypt(String senha){
+		
+		if(senha == null) {
+			return senha;
+		}
+		
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		
+		return bCryptEncoder.encode(senha);
+	}
+	
+	/**
+	 * Verifica se a senha e valida
+	 * 
+	 * @param senha
+	 * @param senhaEncoded
+	 * @return boolean
+	 */
+	public static boolean senhaValida (String senha, String senhaEncoded) {
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.matches(senha, senhaEncoded);
+	}
+}
