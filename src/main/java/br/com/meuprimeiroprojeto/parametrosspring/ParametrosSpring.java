49defb370baf637235190e06a36a7c2c8e3ff6b4
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2019-09-21 19:18:16 -0300

    Modificação do arquivo application.properties

--- src/main/java/br/com/meuprimeiroprojeto/parametrosspring/ParametrosSpring.java
+++ src/main/java/br/com/meuprimeiroprojeto/parametrosspring/ParametrosSpring.java
@@ -1,12 +1,12 @@
-package br.com.meuprimeiroprojeto.parametrosspring;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ParametrosSpring {
-	
-	@Value("${paginacao.qtd_por_pagina}")
-	private int qtdPorPagina;
-	
-}
+//package br.com.meuprimeiroprojeto.parametrosspring;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ParametrosSpring {
+//	
+//	@Value("${paginacao.qtd_por_pagina}")
+//	private int qtdPorPagina;
+//	
+//}
